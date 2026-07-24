e1391b27c5259d9d186df5e81b433afe9d949919
Dan-Brooks <danielbrooks100@icloud.com>
2026-07-24 15:24:16 +0100

    Added comments.

--- Presentation.py
+++ Presentation.py
@@ -1,11 +1,28 @@
+"""
+DJI tello edu Presentation - Dan-Brooks
+
+This is a DJI tello project created for the autonomous systems course in university
+The purpose of this code was to demonstrate the autonomous capabilities of the DJI tello
+
+Requirements
+- OpenCV
+- threading
+
+SDK INFO
+-https://dl-cdn.ryzerobotics.com/downloads/Tello/Tello%20SDK%202.0%20User%20Guide.pdf
+- IP 192.168.10.1
+- UDP PORT 8889
+- VIDEO PORT 11111
+
+"""
+
 import time
 from time import sleep
-
 import cv2
 import socket
 from threading import Thread
-import numpy as np
 
+# Network Configuration
 TELLO_IP = '192.168.10.1'
 TELLO_PORT = 8889
 TELLO_VIDEO_PORT = 11111
@@ -15,17 +32,18 @@ bufferSize = 1024
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 tello_address = (TELLO_IP, TELLO_PORT)
 
-# Send command to Tello
+# Send command to tello and receive a response
 def send_command(command):
     sock.sendto(command.encode('utf-8'), tello_address)
     msg = sock.recvfrom(bufferSize)
     return msg
 
-
+#first command to tello
 msg = send_command("command")
 print(msg)
 
-def Task1():
+#This function demonstrates some basic movement capabilities of the drone
+def task1():
     msg = send_command("takeoff")
     print(msg)
     msg = send_command("up 30")
@@ -45,7 +63,8 @@ def Task1():
     msg = send_command("curve 60 60 0 0 120 0 20")
     print(msg)
 
-def Task2():
+#This function demonstrates more advanced movement and give statistic information
+def task2():
     msg = send_command("takeoff")
     print(msg)
     msg = send_command("flip l")
@@ -67,7 +86,8 @@ def Task2():
     msg = send_command("land")
     print(msg)
 
-def Task3():
+#This function allows a stream of the operations to viewed
+def task3():
     msg = send_command("streamon")
     print(msg)
 
@@ -80,9 +100,10 @@ def Task3():
             if not ret:
                 break
 
-            cv2.imshow('Tello Video Stream', frame)
+            cv2.imshow('tello Video Stream', frame)
 
             cv2.imshow('img1', frame)  # display the captured image
+            # press p to save the current frame
             if cv2.waitKey(1) & 0xFF == ord('p'):
                 timestamp = int(time.time())
                 filename = f"tello_photo_{timestamp}.jpg"
@@ -102,11 +123,12 @@ def Task3():
         print(msg)
         sock.close()
 
+#The threading allows the system to execute task 2 and 3 simultaneously
 def thread():
     if __name__ == '__main__':
-        Thread(target=Task2).start()
-        Thread(target=Task3).start()
+        Thread(target=task2).start()
+        Thread(target=task3).start()
 
-Task1()
+task1()
 sleep(10)
 thread()
